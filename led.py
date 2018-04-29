cee44061cd31d3c16718e6daf9ecff4d30b37715
mete5050 <33295066+mete5050@users.noreply.github.com>
2018-04-29 22:55:49 +0300

    Update led.py

--- led.py
+++ led.py
@@ -2,13 +2,15 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
+ac = "AC"
+KAPA ="KAPA"
 while True:
    x = str(input())
-   if(x == "AC"):
+   if(x == ac):
       GPIO.output(pin,GPIO.HIGH)
       print("Acildi !!!" )
       time.sleep(3)
-   if(x == "KAPA"):
+   if(x == KAPA):
       GPIO.output(pin,GPIO.LOW)
       print("Kapandi !!!")
       time.sleep(1)
